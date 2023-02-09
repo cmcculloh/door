d5d9433c82a23b1a7532b855d0f8462029d5ae0b
cmcculloh <cmcculloh@gmail.com>
2023-02-09 15:38:16 -0500

    Adds Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import Link from 'next/link';
+
+const Header: FC = () => {
+    let isChecked = false;
+    const closeMenu = () => {
+        isChecked = false;
+    };
+
+    return (
+		<header>
+			<div className="topnav">
+				<nav>
+					<h1>Daily Reading</h1>
+					<input id="menu__toggle" type="checkbox" checked={isChecked} />
+					<label className="menu__btn" htmlFor="menu__toggle">
+						<span />
+					</label>
+
+					<ul className="menu__box">
+						<li>
+							<Link className="menu__item" href="/" onClick={closeMenu}>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link className="menu__item" href="/about" onClick={closeMenu}>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link className="menu__item" href="/contact" onClick={closeMenu}>
+								Contact
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+    );
+};
