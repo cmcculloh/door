fbf0ea3746b0841e9e20d827a7bb65d6bb72f96a
cmcculloh <cmcculloh@gmail.com>
2023-02-10 19:59:01 -0500

    Adds in SLDS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "/node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css";
+
+import "@/styles/globals.scss";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
