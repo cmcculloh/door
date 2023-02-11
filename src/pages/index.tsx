07eeb743d21dc0fbc7bab903c6f4caae4548d6f8
cmcculloh <cmcculloh@gmail.com>
2023-02-10 20:02:37 -0500

    Makes Home FC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,55 @@
-import Head from 'next/head'
-import Article from '../components/Article';
+import { FC } from "react";
+import Head from "next/head";
+import Article from "../components/Article";
 
-import { getDailyReadings } from '../lib/readings';
+import { getDailyReadings } from "../lib/readings";
 
 type post = {
-  title: string;
-  date: string;
-  text: string;
-  source: string;
-}
+	title: string;
+	date: string;
+	text: string;
+	source: string;
+};
 
-type data = {
-  posts: post[];
-}
+type dataType = {
+	posts: post[];
+};
 
 type Props = {
-  data: data;
-}
+	data: dataType;
+};
 
 export async function getStaticProps() {
-  const data = await getDailyReadings();
-  return {
-    props: {
-      data
-    }
-  }
+	const data = await getDailyReadings();
+	return {
+		props: {
+			data,
+		},
+	};
 }
 
-export default function Home({ data }) {
-  return (
-    <>
-      <Head>
-        <title>Daily Readings</title>
-        <meta name="description" content="Daily Readings in the Orthodox Church" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        {data.posts.map((post: post, index: number) => (
-          <Article
-            key={index}
-            title={post.title}
-            date={post.date}
-            content={post.text}
-            source={post.source}
-          />
-        ))}
-
-      </main>
-    </>
-  )
+const Home: FC<Props> = ({ data }) => {
+	return (
+		<>
+			<Head>
+				<title>Daily Readings</title>
+				<meta name="description" content="Daily Readings in the Orthodox Church" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				{data.posts.map((post: post, index: number) => (
+					<Article
+						key={index}
+						title={post.title}
+						date={post.date}
+						content={post.text}
+						source={post.source}
+					/>
+				))}
+			</main>
+		</>
+	);
 }
+
+export default Home;
