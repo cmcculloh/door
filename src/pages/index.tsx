cca2051131eb8daf133dbf165b1cbb5b86a5bc02
cmcculloh <cmcculloh@gmail.com>
2023-02-20 17:01:31 -0500

    Adjusts title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Daily Readings</title>
+				<title>Daily Online Orthodox Readings</title>
 				<meta name="description" content="Daily Readings in the Orthodox Church" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
