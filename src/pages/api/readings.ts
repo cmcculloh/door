1209788e6e5fff78d30507261363e2e903a1211d
cmcculloh <cmcculloh@gmail.com>
2023-02-20 20:47:41 -0500

    oops.

--- src/pages/api/readings.ts
+++ src/pages/api/readings.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getFromS3 } from "../../lib/utils/aws.mjs/index.js";
+import { getFromS3 } from "../../lib/utils/aws.mjs";
 
 type Reading = {
 	title: string;
