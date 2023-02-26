048cf918e8d6e34d3e93169b2d8077e43b94c91f
cmcculloh <cmcculloh@gmail.com>
2023-02-26 17:18:29 -0500

    Adds original post date to display

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -16,10 +16,7 @@ const Article: FC<Props> = ({ title, date, contents, source }) => {
     }
     return (
 		<article>
-			<header
-				onClick={() => toggleContent()}
-				className="slds-grid"
-			>
+			<header onClick={() => toggleContent()} className="slds-grid">
 				<h2 className="slds-col">{title}</h2>
 				<Icon
 					classNames="slds-m-top_x-small"
@@ -28,9 +25,12 @@ const Article: FC<Props> = ({ title, date, contents, source }) => {
 				/>
 			</header>
 			<div className={collapsed ? "slds-hide" : ""}>
-				<div dangerouslySetInnerHTML={{__html: contents }} />
+				{/* TODO: Find a way to make it so that if the article contains a relative link, we can make it absolute */}
+				<div dangerouslySetInnerHTML={{ __html: contents }} />
 				<p className="source">
 					Original Source: <a href={source}>{source}</a>
+					<br />
+					Originally Posted: {new Date(date).toISOString().slice(0, 10)}
 				</p>
 			</div>
 		</article>
