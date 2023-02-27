a4bcdedbb0ba8d032958475aed33c3de32c031e4
cmcculloh <cmcculloh@gmail.com>
2023-02-26 21:53:20 -0500

    Updates scraper calls to match new API

--- src/lib/scrapeReadings.mjs
+++ src/lib/scrapeReadings.mjs
@@ -1,12 +1,12 @@
 // fix SyntaxError: Cannot use import statement outside a module
 // https://stackoverflow.com/questions/65097591/node-js-syntaxerror-cannot-use-import-statement-outside-a-module
 
-import scrapeReadings from "./scrapers/scrapeOCADailyReadings.mjs";
-import scrapeCommemorations from "./scrapers/scrapeCommemorations.mjs";
+// import scrapeReadings from "./scrapers/scrapeOCADailyReadings.mjs";
+// import scrapeCommemorations from "./scrapers/scrapeCommemorations.mjs";
 import scrapeSaints from "./scrapers/scrapeSaints.mjs";
-import scrapeOCANews from "./scrapers/scrapeOCANews.mjs";
-import scrapeFarley from "./scrapers/scrapeFarley.mjs";
-import scrapeMorningOffering from "./scrapers/scrapeMorningOffering.mjs";
+// import scrapeOCANews from "./scrapers/scrapeOCANews.mjs";
+// import scrapeFarley from "./scrapers/scrapeFarley.mjs";
+// import scrapeMorningOffering from "./scrapers/scrapeMorningOffering.mjs";
 
 // const today = new Date();
 // const year = today.getFullYear();
@@ -15,11 +15,11 @@ import scrapeMorningOffering from "./scrapers/scrapeMorningOffering.mjs";
 
 // today in YYYY/MM/DD format
 // const todayString = `${year}/${month}/${day}`;
-const todayString = "2024/01/01";
+const todayString = "2023/01/01";
 
-scrapeReadings(todayString, 2024);
-scrapeCommemorations(todayString, 2024);
-// scrapeSaints(todayString, 2024);
+// scrapeReadings();
+// scrapeCommemorations(todayString);
+scrapeSaints();
 // scrapeOCANews();
 // scrapeFarley();
 // scrapeMorningOffering();
